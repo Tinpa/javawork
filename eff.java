bb2e87623d85c4d3ca38744a786eaf6f9c340307
Tinpa <36250538+Tinpa@users.noreply.github.com>
2021-06-03 09:02:24 +0800

    del.ng.prt

--- eff.java
+++
@@ -1,8 +0,0 @@
-/*The maximum possible efficiency of a steam engine depends on the temperature of the steam in the boiler and the temperature of the outside air:
-
-efficiency = 1 - Tair / Tsteam
-where Tair is the air temperature and Tsteam is the steam temperature. The temperatures are give in degrees above absolute zero. Normal air temperature is about 300oK. Boiling is 373oK. Write a program that asks the user for the air temperature and the steam temperature and writes out the maximum possible efficiency of a steam engine. However, if the steam temperature is less than 373oK there is no steam, so the efficiency is zero.
-
-Use integer or floating point input, but do the calculations in floating point.*/
-
-
