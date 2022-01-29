17e757c346ad28eaed88fb70aee81c8d3502cdd8
Tinpa <36250538+Tinpa@users.noreply.github.com>
2022-01-29 15:48:25 +0800

    Update DiscountPrices.java

--- javaSourceCodes/DiscountPrices.java
+++ javaSourceCodes/DiscountPrices.java
@@ -1,7 +1,6 @@
 /*During a sale at a store, a 10% discount is applied to purchases over $10.00. 
 Write a program that asks for the amount of a purchase, then calculates the discounted price. 
 The purchase amount will be input in cents (as an integer):*/
-
 import java.io.*;
 import java.util.Scanner;
 
@@ -13,17 +12,16 @@ class DiscountPrices
    int OriPrice;
    int FinPrice;
       
-   System.out.println( "Enter amount of purchases: ");
+   System.out.println( "Enter amount of purchases(in cents): ");
     OriPrice = scan.nextInt();
     FinPrice = (int) (OriPrice*0.9);
       if(OriPrice<=1000)
           System.out.println( "Your price is :"+OriPrice);
       else 
         {
-         System.out.println("Your discounted price is: "+FinPrice);
-         }
+         System.out.println("Your discounted price (cents) is: "+FinPrice);
+        }
         System.out.println("THANK FOR YOUR PURCHASES!");
         System.out.println("*****TINPA STORE MMXX*****");
   }
 }
-
