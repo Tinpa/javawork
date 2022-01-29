6870639a211fc32bc9beab24901da7353bb25cfe
Tinpa <36250538+Tinpa@users.noreply.github.com>
2022-01-29 15:42:48 +0800

    Update DiscountPrices.java

--- javaSourceCodes/DiscountPrices.java
+++ javaSourceCodes/DiscountPrices.java
@@ -1,35 +1,29 @@
-//During a sale at a store, a 10% discount is applied to purchases over $10.00. Write a program that asks for the amount of a purchase, then calculates the discounted price. The purchase amount will be input in cents (as an integer):
+/*During a sale at a store, a 10% discount is applied to purchases over $10.00. 
+Write a program that asks for the amount of a purchase, then calculates the discounted price. 
+The purchase amount will be input in cents (as an integer):*/
+
 import java.io.*;
 import java.util.Scanner;
 
-
 class DiscountPrices
-{
-    
+{    
   public static void main (String[] args) throws IOException
-  {
-     
-      Scanner scan = new Scanner( System.in );
-      int OriPrice;
+  {    
+   Scanner scan = new Scanner( System.in );
+   int OriPrice;
+   int FinPrice;
       
-      
-      System.out.println( "Enter amount of purchases: ");
-      OriPrice = scan.nextInt();
-      
-      int FinPrice;
-      FinPrice = (int) (OriPrice*0.9);
-      
+   System.out.println( "Enter amount of purchases: ");
+    OriPrice = scan.nextInt();
+    FinPrice = (int) (OriPrice*0.9);
       if(OriPrice<=1000)
-          System.out.println( "Your price are :"+OriPrice);
+          System.out.println( "Your price is :"+OriPrice);
       else 
-    {
-          
-          System.out.println("Your discounted price are: "+FinPrice);
-          
-    }
+        {
+         System.out.println("Your discounted price is: "+FinPrice);
+         }
         System.out.println("THANK FOR YOUR PURCHASES!");
         System.out.println("*****TINPA STORE MMXX*****");
-
   }
 }
 
